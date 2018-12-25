70672a0d1a0735492855523f7d448bf71f3b2f92
Bernice <liuyao0316@outlook.com>
2018-12-25 19:30:16 +0800

    登录完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+import '@/assets/base.css'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/login',
@@ -14,6 +16,22 @@ export default new Router({
     {
       path: '/',
       redirect: '/login'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
+export default router
